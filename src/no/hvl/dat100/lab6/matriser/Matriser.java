5e0172952e10d50edd37fd77e00e101fa84e99a4
602114 <89074496+602114@users.noreply.github.com>
2021-10-04 22:28:48 +0200

    Oppgave 01-B
    Oppgaven om matriser, uten valgfrie oppgaver

--- src/no/hvl/dat100/lab6/matriser/Matriser.java
+++ src/no/hvl/dat100/lab6/matriser/Matriser.java
@@ -1,35 +1,124 @@
 package no.hvl.dat100.lab6.matriser;
 
 public class Matriser {
+	
+public static void main(String[] args) {
+		// Tabellene:
+		int [][] tabell = {
+				{1, 3, 5, 7, 9},
+				{2, 4, 6, 8, 10},
+				{3, 7, 11, 15, 19}
+		};
+		
+		
+		int [][] tabellUlik = {
+				{1, 5, 3, 7, 9},
+				{2, 4, 6, 8, 10},
+				{3, 7, 11, 15, 19}
+		};
+		
+		int [][] tabellRom = {
+				{62, 63, 64, 65, 66},
+				{67, 68, 69, 70, 71},
+		};
+		
+		// Skriver ut oppgavene:
+		System.out.println("---Skriver ut en matrise---");	
+		skrivUt(tabellRom);
+		
+		System.out.println("\n---Skriver ut en matrise som streng---");
+		tilStreng(tabell);
+		
+		System.out.println("\n---Multipliserer alle tall i matrisen med en parameter---");
+		skaler(2, tabell);
+		
+		System.out.println("\n---Ser om to matriser er like---");
+		erLik(tabell, tabell); 
+		
+		System.out.println("\n---Ser om to matriser er like---");
+		erLik(tabell, tabellUlik); 
+	}
 
 	// a)
 	public static void skrivUt(int[][] matrise) {
 		
-		// TODO
-		throw new UnsupportedOperationException("skrivUt ikke implementert");
+		for (int[] rad: matrise) {
+			for (int hm: rad) {
+				System.out.println(hm);
+			}
+			System.out.println();
+		}
 	}
 
 	// b)
 	public static String tilStreng(int[][] matrise) {
-
-		// TODO
-		throw new UnsupportedOperationException("tilStreng ikke implementert");
+	String ut = "";
 		
+		for (int i = 0; i < matrise.length; i++) {
+			int[] rad = matrise[i];
+	
+			for (int j = 0; j < rad.length; j++) {
+				ut += "" + matrise[i][j] + " ";
+			}
+			
+			ut += "\n";
+		}
+		
+		System.out.println(ut);
+		return ut;	
 	}
 
-	// c)
+	// c) 
 	public static int[][] skaler(int tall, int[][] matrise) {
+		int sum = 0;
+		for (int i = 0; i < matrise.length; i++) {
+			int[] rad = matrise[i];
+
+			for (int j = 0; j < rad.length; j++) {
+				sum = matrise[i][j];
+				sum *= tall;
+				System.out.println(sum);	
+			}
+			
+			System.out.println();
+		}
 		
-		// TODO
-		throw new UnsupportedOperationException("skaler ikke implementert");
-	
+		return null;
 	}
 
 	// d)
 	public static boolean erLik(int[][] a, int[][] b) {
-
-		// TODO
-		throw new UnsupportedOperationException("erLik ikke implementert");
+		int lengde = 0;
+		boolean omLik = true;
+		int h = 0;
+		
+		// Sjekker først om de har lik lengde
+		if (a.length > b.length) {
+			omLik = false;
+			
+		} else if (a.length < b.length) {
+			omLik = false;
+		} else
+		lengde = a.length;
+			
+		// Sjekker om tallene i matrisen er like
+		while (h < lengde && omLik == true) {	
+			for (int i = 0; i < lengde; i++) {
+				
+				for (int j = 0; j < lengde; j++) {
+					int talla = a[i][j];
+					int tallb = b[i][j];
+	
+						if (talla != tallb) {
+							omLik = false;
+						} else 
+						h++;
+				}
+			}
+		}
+		
+		System.out.println(omLik);
+		return omLik;
 	}
 	
 	// e)
